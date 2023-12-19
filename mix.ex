5f0ba7af65d8cae56ae2bf3ebb998a29c3aa9b92
RahulGautamSingh <rahultesnik@gmail.com>
2023-12-19 13:35:32 +0545

    Create mix.ex

---
+++ mix.ex
@@ -0,0 +1,30 @@
+defmodule KV.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :kv,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies
+  defp deps do
+    [
+      {:ueberauth, "~> 0.7"},
+      {:ueberauth_github, "~> 0.8"},
+      # Plug required by Ueberauth
+      {:plug, "~> 1.5"},
+    ]
+  end
+end
